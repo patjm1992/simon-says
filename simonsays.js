500401dcaecbdea4522daae804bc1d4b10a6b9f3
patjm1992 <patjm1992@gmail.com>
2016-11-08 11:04:54 -0500

    more responsive audio

--- simonsays.js
+++ simonsays.js
@@ -28,26 +28,37 @@ for (var i = 0; i < 10; i++) {
 }*/
 
 $(document).ready(function() {
-	var note_1 = $('#note_1')[0];
+/*	var note_1 = $('#note_1')[0];
 	var note_2 = $('#note_2')[0];
 	var note_3 = $('#note_3')[0];
-	var note_4 = $('#note_4')[0];
+	var note_4 = $('#note_4')[0];*/
 
-	$("#yellow").click(function() {
+	var note_1 = new Audio("sounds/note1.wav");
+	var note_2 = new Audio("sounds/note2.wav");
+	var note_3 = new Audio("sounds/note3.wav");
+	var note_4 = new Audio("sounds/note4.wav");	
+
+	$("#yellow").mousedown(function() {
 		note_1.play();
+		note_1.currentTime = 0;
 	})
 
-	$("#green").click(function() {
+	$("#green").mousedown(function() {
 		note_2.play();
+		note_2.currentTime = 0;
 	})
 
-	$("#red").click(function() {
+	$("#red").mousedown(function() {
 		note_3.play();
+		note_3.currentTime = 0;
 	})
 
-	$("#blue").click(function() {
+	$("#blue").mousedown(function() {
 		note_4.play();
+		note_4.currentTime = 0;
 	})
+
+	
 
 	function get_random_next() {
 		return Math.floor(Math.random() * 4);
